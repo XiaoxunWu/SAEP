1c5a16af90b312306e05800deb5859833bc6d4d5
XiaoxunWu <132828720+XiaoxunWu@users.noreply.github.com>
2025-11-24 18:36:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ SPACE_AUDIO_FOLDER = "./space"
 ANSWERS_CSV_PATH = os.path.join(AUDIO_FOLDER_REF, "ref_audio.csv")
 SPACE_ANSWERS_CSV_PATH = os.path.join(SPACE_AUDIO_FOLDER, "ref_space.csv")
 # SNR_CSV_PATH = os.path.join(CURRENT_DIR, "SNR_30.csv")#评估页面的空间信息
-DEV_SPACE_PATH = "./our52_5.csv"
+DEV_SPACE_PATH = "./our52_6.csv"
 # 评分保存文件路径
 TEST_AUDIO_CSV_PATH = os.path.join(CURRENT_DIR, "test_audio.csv")
 TEST_SPACE_CSV_PATH = os.path.join(CURRENT_DIR, "test_space.csv")
