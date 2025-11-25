99b0665702cae5a7d010577906ca1ae3da003f9a
XiaoxunWu <132828720+XiaoxunWu@users.noreply.github.com>
2025-11-25 17:39:27 +0800

    Update main_1.py

--- main_1.py
+++ main_1.py
@@ -6,5 +6,5 @@ from gradio_ui2 import create_ui
 if __name__ == "__main__":
     demo = create_ui()
     demo.queue()
-#     # demo.launch(server_name="0.0.0.0", server_port=7860)
-    demo.launch(server_name='127.0.0.1', server_port=7860, inbrowser=True, share=True)
+    demo.launch(server_name="0.0.0.0", server_port=7860)
+    # demo.launch(server_name='127.0.0.1', server_port=7860, inbrowser=True, share=True)
